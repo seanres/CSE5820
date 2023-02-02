31e66b1a9d41b994a1e80a491645a0be41241655
seanres <sean.rescsanski@uconn.edu>
2023-02-02 11:04:50 -0500

    finished code for HW1

--- HW1/train.py
+++ HW1/train.py
@@ -1,12 +1,16 @@
-import random
-import json
+
 import argparse
+import json
+import random
 import time
-from drunkard import Drunkard
-from accountant import Accountant
-from gambler import Gambler
-from dungeon_simulator import DungeonSimulator
+
 import pandas as pd
+from accountant import Accountant
+from drunkard import Drunkard
+from dungeon_simulator import DungeonSimulator
+from gambler import Gambler
+from gambler_hw import GamblerHW
+
 
 def main():
     # parse arguments
@@ -16,6 +20,8 @@ def main():
     parser.add_argument('--discount', type=float, default=0.98, help='Discount for estimated future action')
     parser.add_argument('--iterations', type=int, default=2000, help='Iteration count')
     parser.add_argument('--seed', type=int, default=42, help='For reproducibility')
+    parser.add_argument('--exploration_rate', type=float, default=1.0, help='How much will gambler explore')
+    parser.add_argument('--save_tag', type=str, default='', help='Additional tag to title for renaming')
     
 
     ARGS, unparsed = parser.parse_known_args()
@@ -25,14 +31,17 @@ def main():
     # select agent
     if ARGS.agent == 'GAMBLER':
         agent = Gambler(learning_rate=ARGS.learning_rate, discount=ARGS.discount, iterations=ARGS.iterations)
-        print('gambler')
     elif ARGS.agent == 'ACCOUNTANT':
         agent = Accountant()
+    elif ARGS.agent == 'GAMBLER_HW':
+        agent = GamblerHW(learning_rate=ARGS.learning_rate, discount=ARGS.discount, exploration_rate=ARGS.exploration_rate, iterations=ARGS.iterations)
     else:
         agent = Drunkard()
-        print('picked the drunk!!', ARGS.agent)
 
-
+    if ARGS.agent == 'GAMBLER_HW':
+        step_max = 5
+    else:
+        step_max = 250
 
     # setup simulation
     dungeon = DungeonSimulator()
@@ -50,7 +59,7 @@ def main():
         agent.update(old_state, new_state, action, reward) # Let the agent update internals
 
         total_reward += reward # Keep score
-        if step % 250 == 0: # Print out metadata every 250th iteration
+        if step % step_max == 0: # Print out metadata every 250th iteration
             performance = (total_reward - last_total) / 250.0
             print(json.dumps({'step': step, 'performance': performance, 'total_reward': total_reward}))
             last_total = total_reward
@@ -60,7 +69,7 @@ def main():
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     print("\n")
     print("Final Q-table\n", pd.DataFrame(agent.q_table))
-    pd.DataFrame(save_data).to_csv('simple_RL_'+ARGS.agent+'.csv', header=['Step','Total Reward'],index=False)
+    pd.DataFrame(save_data).to_csv('simple_RL_'+ARGS.agent+ARGS.save_tag+'.csv', header=['Step','Total Reward'],index=False)
 
 if __name__ == "__main__":
     main()
